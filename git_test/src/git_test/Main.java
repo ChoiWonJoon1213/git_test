6311fb8bde8676d0413a8179d161e36b99546016
big2-03 <big2-03@big2-03-PC>
2018-07-10 12:29:36 +0900

    modify : Main class : main()

--- git_test/src/git_test/Main.java
+++ git_test/src/git_test/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hellohello");
 		System.out.println("Hellofick");
+		System.out.println("hi javajvajvaj");
 	}
 
 	public static void main(String[] args) {
