b16e81e82967a1c3fe26251f914a12c18d2534b3
big2-03 <big2-03@big2-03-PC>
2018-07-10 12:07:20 +0900

    merge : complict

--- git_test/src/git_test/Main.java
+++ git_test/src/git_test/Main.java
@@ -1,11 +1,12 @@
 package git_test;
 
 public class Main {
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		System.out.println("Hellohello");
 	}
- public static void main (String[] args) {
-	 System.out.println("Hello java!");
- }
+
+	public static void main(String[] args) {
+		System.out.println("Hello java!");
+	}
 
 }
